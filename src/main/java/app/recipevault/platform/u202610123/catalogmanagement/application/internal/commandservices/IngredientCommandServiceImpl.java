04a156cc6eb05ad94c0a27477cd4cc999f6d8ec1
GZ-99 <gabrielvilchezvite@gmail.com>
2026-07-13 18:35:26 -0500

    chore: seventh commit.

--- src/main/java/app/recipevault/platform/u202610123/catalogmanagement/application/internal/commandservices/IngredientCommandServiceImpl.java
+++ src/main/java/app/recipevault/platform/u202610123/catalogmanagement/application/internal/commandservices/IngredientCommandServiceImpl.java
@@ -1,7 +1,6 @@
 package app.recipevault.platform.u202610123.catalogmanagement.application.internal.commandservices;
 
 import app.recipevault.platform.u202610123.catalogmanagement.application.commandservices.IngredientCommandService;
-import app.recipevault.platform.u202610123.catalogmanagement.application.queryservices.IngredientQueryService;
 import app.recipevault.platform.u202610123.catalogmanagement.domain.model.aggregates.Ingredient;
 import app.recipevault.platform.u202610123.catalogmanagement.domain.model.commands.SeedIngredientsCommand;
 import app.recipevault.platform.u202610123.catalogmanagement.domain.model.commands.UpdateIngredientCommand;
@@ -9,27 +8,24 @@ import app.recipevault.platform.u202610123.catalogmanagement.domain.repositories
 import app.recipevault.platform.u202610123.shared.domain.model.valueobjects.IngredientCode;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class IngredientCommandServiceImpl implements IngredientCommandService {
-
   private final IngredientRepository ingredientRepository;
   public IngredientCommandServiceImpl(IngredientRepository ingredientRepository) {
     this.ingredientRepository = ingredientRepository;
   }
 
-
   @Override
   public void handle(SeedIngredientsCommand command) {
     if (this.ingredientRepository.count() == 0) {
       List<Ingredient> ingredients = List.of(
-          new Ingredient(new IngredientCode("AV-OAT-001"), "Rolled Oats", 40.0, 389.0 ),
-          new Ingredient(new IngredientCode("AV-BER-002"), "Blueberries", 100.0, 57.0 ),
-          new Ingredient(new IngredientCode("AV-PRO-003"), "Whey Protein Powder", 30.0, 400.0 ),
-          new Ingredient(new IngredientCode("AV-NUT-004"), "Almond Milk", 240.0, 15.0 )
+              new Ingredient(new IngredientCode("AV-OAT-001"), "Rolled Oats", 40.0, 389.0 ),
+              new Ingredient(new IngredientCode("AV-BER-002"), "Blueberries", 100.0, 57.0 ),
+              new Ingredient(new IngredientCode("AV-PRO-003"), "Whey Protein Powder", 30.0, 400.0 ),
+              new Ingredient(new IngredientCode("AV-NUT-004"), "Almond Milk", 240.0, 15.0 )
       );
       ingredients.forEach(this.ingredientRepository::save);
     }
@@ -37,6 +33,17 @@ public class IngredientCommandServiceImpl implements IngredientCommandService {
 
   @Override
   public Optional<Ingredient> handle(UpdateIngredientCommand command) {
-    return Optional.empty();
+    IngredientCode code = command.code();
+    Optional<Ingredient> existing = ingredientRepository.findByCode(code);
+
+    if (existing.isEmpty()) {
+      return Optional.empty();
+    }
+
+    Ingredient ing = existing.get();
+    ing.setDefaultPortionGrams(command.defaultPortionGrams());
+
+    Ingredient saved = ingredientRepository.save(ing);
+    return Optional.of(saved);
   }
 }
--- src/main/java/app/recipevault/platform/u202610123/consumptiontracking/application/internal/commandservices/ConsumptionRecordCommandServiceImpl.java
+++ src/main/java/app/recipevault/platform/u202610123/consumptiontracking/application/internal/commandservices/ConsumptionRecordCommandServiceImpl.java
@@ -1,8 +1,10 @@
 package app.recipevault.platform.u202610123.consumptiontracking.application.internal.commandservices;
 
+import app.recipevault.platform.u202610123.consumptiontracking.application.acl.CatalogACL;
 import app.recipevault.platform.u202610123.consumptiontracking.application.commandservices.ConsumptionRecordCommandService;
 import app.recipevault.platform.u202610123.consumptiontracking.domain.model.aggregates.ConsumptionRecord;
 import app.recipevault.platform.u202610123.consumptiontracking.domain.model.commands.CreateConsumptionRecordCommand;
+import app.recipevault.platform.u202610123.consumptiontracking.domain.model.events.ConsumptionRecordRegisteredEvent;
 import app.recipevault.platform.u202610123.consumptiontracking.domain.repositories.ConsumptionRecordRepository;
 import app.recipevault.platform.u202610123.shared.application.result.ApplicationError;
 import app.recipevault.platform.u202610123.shared.application.result.Result;
@@ -10,22 +12,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumptionRecordCommandServiceImpl implements ConsumptionRecordCommandService {
-
   private final ConsumptionRecordRepository consumptionRecordRepository;
-  public ConsumptionRecordCommandServiceImpl(ConsumptionRecordRepository consumptionRecordRepository) {
+  private final CatalogACL catalogACL;
+  public ConsumptionRecordCommandServiceImpl(ConsumptionRecordRepository consumptionRecordRepository,
+                                             CatalogACL catalogACL) {
     this.consumptionRecordRepository = consumptionRecordRepository;
+    this.catalogACL = catalogACL;
   }
 
   @Override
   public Result<Long, ApplicationError> handle(CreateConsumptionRecordCommand command) {
     var ingredientCode = command.ingredientCode();
-    //- falta ACL
+    var ingredient = catalogACL.getIngredientData(command.ingredientCode());
     var consumptionRecord = new ConsumptionRecord(command);
-    try {
-      consumptionRecord = this.consumptionRecordRepository.save(consumptionRecord);
-    } catch (Exception e) {
-      return Result.failure(ApplicationError.unexpected("consumption-record", e.getMessage()));
+    if (ingredient.isPresent()) {
+      try {
+        consumptionRecord = this.consumptionRecordRepository.save(consumptionRecord);
+      } catch (Exception e) {
+        return Result.failure(ApplicationError.unexpected("consumption-record", e.getMessage()));
+      }
+      return Result.success(consumptionRecord.getId());
     }
-    return Result.success(consumptionRecord.getId());
+    else {
+      return Result.failure(ApplicationError.notFound("ingredient", ingredientCode.code()));
+    }
   }
 }

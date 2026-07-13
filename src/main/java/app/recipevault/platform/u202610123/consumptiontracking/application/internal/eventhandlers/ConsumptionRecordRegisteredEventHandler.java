cb089989759294a97fa6107ed51bc0973b298143
GZ-99 <gabrielvilchezvite@gmail.com>
2026-07-13 11:29:45 -0500

    chore: add first homework.

--- src/main/java/app/recipevault/platform/u202610123/consumptiontracking/application/internal/eventhandlers/ConsumptionRecordRegisteredEventHandler.java
+++ src/main/java/app/recipevault/platform/u202610123/consumptiontracking/application/internal/eventhandlers/ConsumptionRecordRegisteredEventHandler.java
@@ -1,6 +1,9 @@
 package app.recipevault.platform.u202610123.consumptiontracking.application.internal.eventhandlers;
 
-import app.recipevault.platform.u202610123.consumptiontracking.application.commandservices.ConsumptionRecordCommandService;
+import app.recipevault.platform.u202610123.catalogmanagement.application.commandservices.IngredientCommandService;
+import app.recipevault.platform.u202610123.catalogmanagement.domain.model.commands.UpdateIngredientCommand;
+import app.recipevault.platform.u202610123.catalogmanagement.domain.repositories.IngredientRepository;
+import app.recipevault.platform.u202610123.consumptiontracking.domain.model.events.ConsumptionRecordRegisteredEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,16 +15,39 @@ import java.sql.Timestamp;
 @Service
 @Slf4j
 public class ConsumptionRecordRegisteredEventHandler {
-    private final ConsumptionRecordCommandService consumptionCommandService;
+    private final IngredientRepository ingredientRepository;
+    private final IngredientCommandService ingredientCommandService;
+
     private static final Logger LOGGER
       = LoggerFactory.getLogger(ConsumptionRecordRegisteredEventHandler.class);
 
-    public ConsumptionRecordRegisteredEventHandler(ConsumptionRecordCommandService consumptionCommandService) {
-        this.consumptionCommandService = consumptionCommandService;
+    public ConsumptionRecordRegisteredEventHandler(IngredientRepository ingredientRepository,
+                                                   IngredientCommandService ingredientCommandService) {
+        this.ingredientRepository = ingredientRepository;
+        this.ingredientCommandService = ingredientCommandService;
     }
 
-    //@EventListener
-    //public void on() {
+    @EventListener
+    public void on(ConsumptionRecordRegisteredEvent event) {
+        if (ingredientRepository.existsByCode(event.ingredientCode())) {
+            var ingredient = ingredientRepository.findByCode(event.ingredientCode());
+            var ing = ingredient.get();
+            if (event.portionGrams() < ing.getDefaultPortionGrams()) {
+                var updateIngredientCommand = new UpdateIngredientCommand(ing.getId(),
+                        ing.getCode(), ing.getName(), event.portionGrams(), ing.getBaseCaloriesPer100g());
+                this.ingredientCommandService.handle(updateIngredientCommand);
+                LOGGER.info("Process of Updating Portion Grams per serving completed in {}",
+                        currentTimestamp());
+            }
+            else {
+                LOGGER.info("The New Portion Grams value is invalid");
+            }
+        } else {
+            LOGGER.info("The Ingredient does not exist");
+        }
+    }
 
-    //}
+    private Timestamp currentTimestamp() {
+        return new Timestamp(System.currentTimeMillis());
+    }
 }
